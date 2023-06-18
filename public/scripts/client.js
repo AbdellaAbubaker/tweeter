a81f68b7e0e4613117381d068c31068d0ce99a2a
AbdellaAbubaker <aabdella.abubaker@mgail.com>
2023-06-18 15:10:00 -0400

    fixing bugs/clearing tweet-area and resetting counter

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -6,8 +6,12 @@
 
 
 const createTweetElement = function (tweet) {
-    const { user, content, created_at } = tweet;
-    const $tweet = $(`
+  const {
+    user,
+    content,
+    created_at
+  } = tweet;
+  const $tweet = $(`
     <article class="tweet">
     <header>
       <div class="user-info">
@@ -38,75 +42,100 @@ const createTweetElement = function (tweet) {
   </article>
 
     `);
-    return $tweet;
-  };
+  return $tweet;
+};
 
-  
-  
-  const renderTweets = function (tweets) {
-    tweets.forEach((tweet) => {
-      const $tweet = createTweetElement(tweet);
-      $('.tweets-container').prepend($tweet);
-    });
-  };
-  
-  const loadTweets = function () {
-    $.ajax('/tweets', { method: 'GET', dataType: 'JSON', success: renderTweets });
-  };
-  
-  $(document).ready(function () {
-    $("form").submit(function (event) {
-      event.preventDefault();
-  
-      const textarea = $("#tweet-text");
-      const content = textarea.val();
-      if (!content || content.trim() === "") {
-        showError("Please enter a tweet");
-        return;
+
+
+const renderTweets = function (tweets) {
+  $('.tweets-container').empty();
+  tweets.forEach((tweet) => {
+    const $tweet = createTweetElement(tweet);
+    $('.tweets-container').prepend($tweet);
+  });
+};
+
+const loadTweets = function () {
+  $.ajax('/tweets', {
+    method: 'GET',
+    dataType: 'JSON',
+    success: renderTweets
+  });
+};
+
+$(document).ready(function () {
+  $("form").submit(function (event) {
+    event.preventDefault();
+
+    const textarea = $("#tweet-text");
+    const content = textarea.val();
+    if (!content || content.trim() === "") {
+      showError("Please enter a tweet");
+      return;
+    }
+
+    if (content.length > 140) {
+      showError("Tweet content is too long. Maximum allowed is 140 characters.");
+      return;
+    }
+    clearError();
+
+    var formData = $(this).serialize();
+
+    $.ajax({
+      type: "POST",
+      url: "/tweets",
+      data: formData,
+      success: function (data) {
+        $("#tweet-text").val("");
+        $(".counter").text("140");
+        clearError();
+        loadTweets();
+       
+      },
+      error: function (xhr, status, error) {
+        showError("An error occurred while submitting the tweet. Please try again later.");
       }
       
-      if (content.length > 140) {
-        showError("Tweet content is too long. Maximum allowed is 140 characters.");
-        return;
-      }
-      clearError();
-  
-      var formData = $(this).serialize();
-  
-      $.ajax({
-        type: "POST",
-        url: "/tweets",
-        data: formData,
-        success: function (data) {
-          loadTweets();
-        },
-        error: function (xhr, status, error) {
-          showError("An error occurred while submitting the tweet. Please try again later.");
-        }
-      });
     });
-  
-    loadTweets();
-    $(".write-tweet").click(function(){
-      $(".new-tweet").slideDown()
-    }) 
-
-    
   });
-  
-  function showError(message) {
-    const $errorContainer = $(".error-container");
-    $errorContainer.text(message);
-    $errorContainer.slideDown(); // Show the error container with sliding animation
-  }
-  
-  function clearError() {
-    const $errorContainer = $(".error-container");
-    $errorContainer.text("");
-    $errorContainer.hide(); // Hide the error container
-  }
-  
- 
+
+  loadTweets();
+  $(".write-tweet").click(function () {
+    $(".new-tweet").slideDown()
+  })
+  $("#tweet-text").on('input', function () {
+    const remainningChars = 140 - $(this).val().length;
+    if (remainningChars >= 0) {
+      clearError();
+    }
+  })
+
+});
+
+
+
+function showError(message) {
+  const $errorContainer = $(".error-container");
+  $errorContainer.text(message);
+  $errorContainer.slideDown(); // Show the error container with sliding animation
+}
+
+function clearError() {
+  const $errorContainer = $(".error-container");
+  $errorContainer.text("");
+  $errorContainer.hide(); // Hide the error container
+}
+
+$("#arrow-animation").click(function () {
+  console.log("clicked")
+  $(this).effect("highlight", {}, 3000);
+
+});
+
+
+
+// Debugging tips
 // 1. Targeting right element 
 // 2. Make sure there are typo 
 // 3. Following fibction , check what calls first up the function chain 
